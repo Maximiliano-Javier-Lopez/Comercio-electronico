50a95d14c98dd213e0083a653412c979dc4f28d3
MaximilianoLopez <31918166@itbeltran.com.ar>
2024-09-13 18:13:30 -0300

    arreglos general

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-  <title>Simulación de Elemento</title>
-  <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de Electrónica</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
 </head>
 <body>
-  <div class="container">
-    <h2 class="titlulo">Engage Jupiter </h2>
-    <p class="subtitle">Engage Jupiter Express for outer solar system travel</p>
-    <div class="actions">
-      <img src="img.jpg" class="img-flecha" alt=""/>
-      <img src="tilde.jpg" class="img-tilde" alt=""/>
-      <label class="responsable">Responsable</label>
-    </div>
-    <div class="info">
-      <span class="number">Carrera</span>
-      <input type="text" alt="Avatar" value="<?php echo 'ML' ?>" class="avatar">
-    </div>
-  </div>
+
+<?php include 'include/nabvar.php';
+include 'include/header.php';
+include 'productos/index.php'; 
+include 'include/footer.php'?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="script.js"></script>
 </body>
 </html>
